45b31e365f93dc63ea1737d1c86a0e025b6b6dc5
ShenghaoisYummy <hsupisces@hotmail.com>
2024-02-14 09:30:36 +1100

    register completed

--- Nodejs login /backend/src/register.js
+++ Nodejs login /backend/src/register.js
@@ -1,4 +1,45 @@
-export function register(req, res) {
-  console.log(req.body);
-  res.send("Register");
+import axios from "axios";
+import "dotenv/config";
+
+import { getUsers } from "./utils/getUsers.js";
+import { userDBPath } from "./utils/dataPath.js";
+
+export async function register(req, res) {
+  const { username, password } = req.body;
+  try {
+    const users = await getUsers();
+
+    if (!(users && Array.isArray(users) && users.length > 0)) {
+      res.status(500).json({ message: "No user found", code: 2 });
+      return;
+    } else {
+      const user = users.find((user) => user.username === username);
+
+      if (user) {
+        res.status(409).json({
+          message: "User already exists",
+          code: "1",
+          username: username,
+        });
+        return;
+      } else if (!password) {
+        res.status(409).json({
+          message: "Password is empty",
+          code: "2",
+          username: username,
+        });
+        return;
+      } else {
+        await axios.post(userDBPath, {
+          username: username,
+          password: password,
+        });
+        res
+          .status(200)
+          .json({ message: "Register success", code: "0", username: username });
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
